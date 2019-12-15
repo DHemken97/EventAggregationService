0080193a03a2752cc3e1e6002d8ac1f143c3a32f
d1108 <d1108@DESKTOP-ES2M0UJ>
2019-12-15 09:14:18 -0600

    Build CorePlugin Version 1.0.0.0

--- EventAggregationServiceHost/Program.cs
+++ EventAggregationServiceHost/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.ServiceProcess;
+﻿using System;
+using System.IO;
+using System.Runtime.ExceptionServices;
+using System.ServiceProcess;
 
 namespace EventAggregationServiceHost
 {
@@ -10,11 +13,22 @@ namespace EventAggregationServiceHost
         static void Main()
         {
             ServiceBase[] ServicesToRun;
+            SetupErrorHandling();
             ServicesToRun = new ServiceBase[]
             {
                 new Service1()
             };
             ServiceBase.Run(ServicesToRun);
+        }
+
+        private static void SetupErrorHandling()
+        {
+            AppDomain.CurrentDomain.FirstChanceException += FirstChanceHandler;
+        }
+
+        private static void FirstChanceHandler(object sender, FirstChanceExceptionEventArgs e)
+        {
+            File.AppendAllText($@"C:\Users\d1108\Projects\EventAggregationService\EventAggregationServiceHost\bin\Debug\Errors\{e.Exception.GetType().Name}.txt", e.Exception.Message);
         }
     }
 }
