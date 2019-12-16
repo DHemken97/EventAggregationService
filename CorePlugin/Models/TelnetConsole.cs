73609b18723eb756210810f596487256c551a8d5
d1108 <d1108@DESKTOP-ES2M0UJ>
2019-12-15 20:40:57 -0600

    Added Package Manager List Functions

---
+++ CorePlugin/Models/TelnetConsole.cs
@@ -0,0 +1,22 @@
+﻿using EAS_Development_Interfaces.Interfaces;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CorePlugin.Models
+{
+    public class TelnetConsole : IConsole
+    {
+        private NetworkStream NetworkStream;
+        public TelnetConsole(NetworkStream stream)
+        {
+            NetworkStream = stream;
+
+        }
+        public void Write(string value)
+        {
+            var msg = Encoding.ASCII.GetBytes(value);
+
+            NetworkStream.Write(msg, 0, msg.Length);
+        }
+    }
+}
---
+++ PluginManager/Helpers/HttpHelper.cs
@@ -0,0 +1,25 @@
+﻿using EAS_Development_Interfaces;
+using System.IO;
+using System.Net;
+
+namespace PluginManager.Helpers
+{
+    public static class HttpHelper
+    {
+        public static TType Get<TType>(string URL)
+        {
+            string stringResponse;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                stringResponse = reader.ReadToEnd();
+            }
+            return stringResponse.FromJson<TType>();
+        }
+    }
+}
