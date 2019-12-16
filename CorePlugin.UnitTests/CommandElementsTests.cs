27d4316224fda288e0020e08947de9c347ef1850
d1108 <d1108@DESKTOP-ES2M0UJ>
2019-12-15 18:29:52 -0600

    Build CorePlugin Version 1.0.0.0

---
+++ CorePlugin.UnitTests/CommandElementsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EAS_Development_Interfaces.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CorePlugin.UnitTests
+{
+    [TestClass]
+    public class CommandElementsTests
+    {
+        
+        [DataTestMethod]
+        [DataRow("Test Command","Test",new[] { "Command"},new char[0],default(Dictionary<string,string>))]
+        public void CommandElementsShouldParseCommandProperly(string command, string op, string[] args, char[] flags,Dictionary<string,string> doubleFlags)
+        {
+            var sut = new CommandElements(command);
+            Assert.AreEqual(sut.command, op);
+            Assert.IsTrue(sut.Arguments.SequenceEqual(args));
+            Assert.IsTrue(sut.Flags.OrderBy(flag => flag).SequenceEqual(args.OrderBy(flag => flag)));
+            Assert.IsTrue(sut.DoubleFlags.SequenceEqual(doubleFlags));
+
+        }
+    }
+}
---
+++ CorePlugin.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CorePlugin.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CorePlugin.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf652cd4-d149-412b-b908-ccb54bb1a85b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
