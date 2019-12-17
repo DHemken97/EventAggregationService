403ea7ff2cf0eb96e101a096060afd92e3087623
DHemken <DHemken@wardkraft.com>
2019-12-17 15:01:15 -0600

    Build CorePlugin Version 1.0.0.0

--- EAS_Development_Interfaces/Models/Configuration.cs
+++ EAS_Development_Interfaces/Models/Configuration.cs
@@ -48,7 +48,13 @@ namespace EAS_Development_Interfaces
 
         public static void Reload()
         {
-            Task.Run(() => Process.Start("CMD net stop EAS && net Start EAS"));
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "net stop EAS && net Start EAS";
+            process.StartInfo = startInfo;
+            process.Start();
         }
         private static void LoadBootstrappers()
         {
