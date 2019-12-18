97f50ab278067ba2be52b46c2b59253fd22d84db
DHemken <DHemken@wardkraft.com>
2019-12-18 09:37:28 -0600

    Build CorePlugin Version 1.0.0.0

--- EAS_Development_Interfaces/Models/Configuration.cs
+++ EAS_Development_Interfaces/Models/Configuration.cs
@@ -67,8 +67,7 @@ namespace EAS_Development_Interfaces
             var name = Path.GetFileNameWithoutExtension(path);
             var _appDomain = AppDomain.CreateDomain(name);
             var runtimePath = $@"{BaseDirectory}\{name}.dll";
-          //  File.Copy(path,runtimePath);
-                _appDomain.Load(runtimePath);
+                _appDomain.Load(AssemblyName.GetAssemblyName(runtimePath));
                 return _appDomain;
 
         }
